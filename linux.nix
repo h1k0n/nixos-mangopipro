42bd2d530c58ea3b32f2c08dd863961028d66453
h1k0n <ybzjyhk@gmail.com>
2024-07-27 21:26:22 +0800

    c906 v0p7

--- linux.nix
+++ linux.nix
@@ -26,6 +26,10 @@ linuxKernel.manualConfig {
 
   configfile = ./68.config;
   allowImportFromDerivation = true;
+  extraMakeFlags = [
+    "KCFLAGS+=-march=rv64gc_v0p7_zihintpause"
+    "KCFLAGS+=-mcpu=thead-c906"
+  ];
 }
 ).overrideAttrs (old: {
   name = "k"; # shorten the kernel name, dodge uboot length limits, otherwise it will make uboot fail to load kernel. 
