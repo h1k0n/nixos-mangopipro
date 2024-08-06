6b2cd2e3eeface071ff849e6bcb41ce250186340
h1k0n <ybzjyhk@gmail.com>
2024-08-06 09:21:40 +0800

    成功编译

--- uboot.nix
+++ uboot.nix
@@ -13,7 +13,7 @@ buildUBoot {
     repo = "u-boot";
     # Last git revision from the `d1-wip` branch:
     rev = "2e89b706f5c956a70c989cd31665f1429e9a0b48";
-    sha256 = "";
+    sha256 = "sha256-POjP3PPuluYNTWWo5EUFWT0K3zYFWBFviPOGIhnejCA=";
   };
   patches = [
   ];
