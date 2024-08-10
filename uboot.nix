9fd186aedcfc30b87cb72e84f41e899caaabc2fe
h1k0n <ybzjyhk@gmail.com>
2024-08-10 11:12:32 +0800

    invalid instruction

--- uboot.nix
+++ uboot.nix
@@ -6,14 +6,14 @@
 }:
 
 buildUBoot {
-  version = "d1-2022-10-31";
+  version = "d1-wip";
 
   src = fetchFromGitHub {
     owner = "smaeul";
     repo = "u-boot";
     # Last git revision from the `d1-wip` branch:
-    rev = "329e94f16ff84f9cf9341f8dfdff7af1b1e6ee9a";
-    sha256 = "sha256-c4yHizDvfRTqnxyKzNrSPCdlesBWuzgyQIEhpR690Vc=";
+    rev = "2e89b706f5c956a70c989cd31665f1429e9a0b48";
+    sha256 = "sha256-POjP3PPuluYNTWWo5EUFWT0K3zYFWBFviPOGIhnejCA=";
   };
   patches = [
     ./uboot.patch
