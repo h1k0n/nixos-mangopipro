edd99aed0e1d03d70ec13f13ddbfb61a6492fd51
h1k0n <ybzjyhk@gmail.com>
2024-08-10 10:16:47 +0800

    v enable successful boot

--- linux.nix
+++ linux.nix
@@ -35,10 +35,6 @@ linuxKernel.manualConfig {
 
   configfile = ./68xthead.config;
   allowImportFromDerivation = true;
-  extraMakeFlags = [
-    "KCFLAGS+=-march=rv64gc_xtheadvector_zihintpause"
-    "KCFLAGS+=-mcpu=thead-c906"
-  ];
 }
 ).overrideAttrs (old: {
   name = "k"; # shorten the kernel name, dodge uboot length limits, otherwise it will make uboot fail to load kernel. 
