f050c600e210b7de3355a7f52e69f21f60432fd8
h1k0n <ybzjyhk@gmail.com>
2024-08-26 15:33:14 +0800

    remove extraMakeFlags build pass

--- linux.nix
+++ linux.nix
@@ -29,10 +29,6 @@ linuxKernel.manualConfig {
 
   configfile = ./68.config;
   allowImportFromDerivation = true;
-  extraMakeFlags = [
-    "KCFLAGS+=-march=rv64gc_xtheadvector_zihintpause"
-    "KCFLAGS+=-mcpu=thead-c906"
-  ];
 }
 ).overrideAttrs (old: {
   name = "k"; # shorten the kernel name, dodge uboot length limits, otherwise it will make uboot fail to load kernel. 
