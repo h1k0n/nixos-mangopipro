28914c546a1adbaef0c74b28e3d361ddf4e8c256
h1k0n <ybzjyhk@gmail.com>
2024-08-26 11:44:05 +0800

    rm kvm config

--- uboot.nix
+++ uboot.nix
@@ -7,17 +7,17 @@
 }:
 
 (buildUBoot {
-  version = "d1-wip";
+  version = "d1-2022-10-31";
 
   src = fetchFromGitHub {
     owner = "smaeul";
     repo = "u-boot";
     # Last git revision from the `d1-wip` branch:
-    rev = "2e89b706f5c956a70c989cd31665f1429e9a0b48";
-    sha256 = "sha256-POjP3PPuluYNTWWo5EUFWT0K3zYFWBFviPOGIhnejCA=";
+    rev = "329e94f16ff84f9cf9341f8dfdff7af1b1e6ee9a";
+    sha256 = "sha256-c4yHizDvfRTqnxyKzNrSPCdlesBWuzgyQIEhpR690Vc=";
   };
 
-  defconfig = "lichee_rv_dock_defconfig";
+  defconfig = "nezha_defconfig";
   extraMeta.platforms = [ "riscv64-linux" ];
   extraMakeFlags = [
     "OPENSBI=${opensbi}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin"
