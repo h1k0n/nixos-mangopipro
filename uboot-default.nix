b618fc5e783fbffa89ebd81404cd6a8b57661eb4
h1k0n <ybzjyhk@gmail.com>
2024-10-17 22:07:12 +0800

    uboot d1-wip不行;uboot2022行

--- uboot-default.nix
+++ uboot-default.nix
@@ -656,8 +656,8 @@ ubootD1 = buildUBoot {
     owner = "smaeul";
     repo = "u-boot";
     # Last git revision from the `d1-wip` branch:
-    rev = "2e89b706f5c956a70c989cd31665f1429e9a0b48";
-    sha256 = "sha256-POjP3PPuluYNTWWo5EUFWT0K3zYFWBFviPOGIhnejCA=";
+    rev = "329e94f16ff84f9cf9341f8dfdff7af1b1e6ee9a";
+    sha256 = "sha256-c4yHizDvfRTqnxyKzNrSPCdlesBWuzgyQIEhpR690Vc=";
   };
   patches = [
   ];
