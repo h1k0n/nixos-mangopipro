d8c363bb8a1c7bffa11ace03abfd1e5240606e49
h1k0n <ybzjyhk@gmail.com>
2025-04-09 10:32:21 +0800

    NixOS/nixpkgs e3e32b642a3

--- linux.nix
+++ linux.nix
@@ -15,7 +15,7 @@
     modVersion = "6.14.1";
     src = fetchurl {
       url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.14.1.tar.gz";
-      sha256 = "sha256-wPR5uEM1knyl+FsXu/s/aFcsEtpYJJ2b2VFC/iuhOV0=";
+      sha256 = "sha256-bdczqkWtayA7hTHBaly8UMycC27HShDoLrU2gSWCjr0=";
     };
   in
   # kernelStdenv = overrideCC stdenv buildPackages.gcc14;
