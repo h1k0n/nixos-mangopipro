62acb7eca8c790dfdcc9884401d3c1ebd73ae1b2
System administrator <root@vultr.guest>
2026-04-09 15:03:56 +0000

    multi

--- sd-image-btrfs.nix
+++ sd-image-btrfs.nix
@@ -209,7 +209,17 @@ in
         "/" = {
           device = "/dev/disk/by-label/NIXOS_SD";
           fsType = "btrfs";
-          options = [ "noatime" "compress=zstd" ];
+          options = [ "noatime" "compress=zstd" "subvol=/@"  ];
+        };
+        "/boot" = {
+          device = "/dev/disk/by-label/NIXOS_SD";
+          fsType = "btrfs";
+          options = [ "noatime" "compress=zstd" "subvol=/@boot" ];
+        };
+        "/nix" = {
+          device = "/dev/disk/by-label/NIXOS_SD";
+          fsType = "btrfs";
+          options = [ "noatime" "compress=zstd" "subvol=/@nix" ];
         };
     };
 
