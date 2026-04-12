2526a6843caa59e0de5fabe99b3cd6d42c51e0fa
System administrator <root@vultr.guest>
2026-04-12 14:30:24 +0000

    @boot nofail

--- sd-image-btrfs.nix
+++ sd-image-btrfs.nix
@@ -218,12 +218,13 @@ in
         "/" = {
           device = "/dev/disk/by-label/NIXOS_SD";
           fsType = "btrfs";
+          autoResize = config.sdImage.expandOnBoot;
           options = [ "noatime" "compress=zstd" "subvol=@"  ];
         };
         "/boot" = {
           device = "/dev/disk/by-label/NIXOS_SD";
           fsType = "btrfs";
-          options = [ "noatime" "compress=zstd" "subvol=@boot"  ];
+          options = [ "noatime" "compress=zstd" "subvol=@boot" "nofail" "x-systemd.device-timeout=1s"  ];
         };
         "/nix" = {
           device = "/dev/disk/by-label/NIXOS_SD";
@@ -234,6 +235,7 @@ in
       };
 
     sdImage.storePaths = [ config.system.build.toplevel ];
+    boot.growPartition = config.sdImage.expandOnBoot;
 
     system.build.sdImage = pkgs.callPackage (
       {
@@ -346,17 +348,6 @@ in
 
     boot.postBootCommands =
       let
-        expandOnBoot = lib.optionalString config.sdImage.expandOnBoot ''
-          # Figure out device names for the boot device and root filesystem.
-          rootPart=$(${pkgs.util-linux}/bin/findmnt -n -o SOURCE /)
-          bootDevice=$(lsblk -npo PKNAME $rootPart)
-          partNum=$(lsblk -npo MAJ:MIN $rootPart | ${pkgs.gawk}/bin/awk -F: '{print $2}')
-
-          # Resize the root partition and the filesystem to fit the disk
-          echo ",+," | sfdisk -N$partNum --no-reread $bootDevice
-          ${pkgs.parted}/bin/partprobe
-          ${pkgs.btrfs-progs}/bin/btrfs filesystem resize max /
-        '';
         nixPathRegistrationFile = config.sdImage.nixPathRegistrationFile;
       in
       ''
@@ -364,8 +355,6 @@ in
         if [ -f ${nixPathRegistrationFile} ]; then
           set -euo pipefail
           set -x
-
-          ${expandOnBoot}
 
           # Register the contents of the initial Nix store
           ${config.nix.package.out}/bin/nix-store --load-db < ${nixPathRegistrationFile}
